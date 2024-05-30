e7d7ac8da275fb76fa4a6641f101fb583d8c6ffd
Nguyễn Kim Quốc Nam <namnkq2603@gmail.com>
2024-05-30 13:14:21 +0700

    config: implement environment variables

---
+++ server/src/config/environment.js
@@ -0,0 +1,7 @@
+import 'dotenv/config'
+
+export const env = {
+  AUTHOR: process.env.AUTHOR,
+  APP_HOST: process.env.APP_HOST,
+  APP_PORT: process.env.APP_PORT
+}
--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,13 @@
 import express from 'express'
+import { env } from './config/environment'
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World, I\'m QuocNamDev Testasasa<h1>')
+  res.send('<h1>Hello World, I\'m QuocNamDev<h1>')
 })
 
-const hostname = 'localhost'
-
-const port = 8017
-
-
-app.listen(port, hostname, () => {
+app.listen(env.APP_PORT, env.APP_HOST, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server is running at http://${hostname}:${port}`)
+  console.log(`Server is running at http://${env.APP_HOST}:${env.APP_PORT} -- Author: ${env.AUTHOR}`)
 })
